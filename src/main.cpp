5a9a7979e5fe75bd98fedb65cbb3769a691f4a86
Josh Stechnij <jstechnij@flexerasoftware.com>
2020-12-07 23:00:49 -0600

    Add sorting

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ int main(int argc, char **argv)
 	std::vector<std::string> vecTest;
 	vecTest.push_back("Hello");
 	vecTest.push_back("world");
+
+	std::sort(vecTest.begin(), vecTest.end());
 	
 	printf("hello world from github\n");
 	printf("new line\n");
