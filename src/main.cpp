c69bb8dea8f5a4dfbf6ac1027fc4b05da77d0e8c
Josh Stechnij <jstechnij@flexerasoftware.com>
2020-12-07 23:03:31 -0600

    Initial work on new_feature

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,18 @@
 #include <stdlib.h>
 #include <string>
 
+void new_feature()
+{
+	printf("Hello this is new feature...\n");
+}
+
 int main(int argc, char **argv)
 {
 	printf("hello world from github\n");
 	printf("new line\n");
+
+	new_feature();
+
 	return 0;
 }
 
